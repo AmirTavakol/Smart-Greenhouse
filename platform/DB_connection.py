cda7b4e013a52e35c3a3c535951af2bc8fba4e10
Francesco Tosetti <frc.tst97@gmail.com>
2021-04-01 22:33:08 +0200

    db_connect.py fixed

--- platform/DB_connection.py
+++ platform/DB_connection.py
@@ -1,3 +1,20 @@
 import mysql.connector
 from mysql.connector import errorcode
 
+def connect():
+    # %% Connect to database, raise error if any
+    try:
+        mydb = mysql.connector.connect(user='sslroot',
+                                       password='idrom3sh..',
+                                       host='80.210.98.95',
+                                       database='MeshliumDB')
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your username or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+
+    return mydb
