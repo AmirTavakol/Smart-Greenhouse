a3a5764edb485d50a470ca354327e54b092c2e98
Francesco Tosetti <frc.tst97@gmail.com>
2021-04-25 18:17:00 +0200

    OOP paradigm, updated

--- platform/ET0.py
+++ platform/ET0.py
@@ -5,11 +5,15 @@
 #TODO: integration of es and ea values
 
 import json
+import dbconnection
 import ET0_evaluation
 import math
 import select_mysql
 import R_n
+import ast
+from pprint import pprint
 
+"""
 def delta(T_mean):
     '∆ slope vapour pressure curve [kPa °C-1]'
     #actually now we are passing T and not Tmean, we should work on this
@@ -20,13 +24,13 @@ def gamma(atm_press):
     'psychrometric constant'
     gamma = 0.000665 * atm_press
     return gamma
-
+"""
 with open('ET0_values.json') as json_file:
     data = json.load(json_file)
 
 
 if __name__ == '__main__':
-
+    '''
     "first of all get the last measured values"
     select_mysql
 
@@ -36,15 +40,58 @@ if __name__ == '__main__':
     atm_press = values['PRES']
     Rs = values['PAR']
 
-    #evalauting coefficients
-    delta = delta(T)
-    gamma = gamma(atm_press)
-    R_n = R_n.R_n(Rs)
+    '''
 
-    print(values)
-    ET0 = ET0_evaluation.ET0(delta, R_n, gamma, T, wind = 0, es=4, ea=0 )
+    
 
-    print(ET0)
+    db = dbconnection.db_connection()
+    file = db.getDataForParameter()
+    dict = ast.literal_eval(file)
+    Tmax = dict['max_tc']
+    Tmin = dict['min_tc']
+    RHmax = dict['max_hum']
+    RHmin = dict['min_hum']
+    par_avg = dict['avg_solar']
+
+    print("Over the last 24h, the maximum temperature was: " + str(Tmax) +"\n the minimum temperature was: "+ str(Tmin)+ " \n while RH minimum: "+str(RHmin)+" and RH Maximum: "+ str(RHmax) +" \n the average solar radiation was:" + str(par_avg))
+
+    values = db.getDataLastEpoch()
+    atm_press = float(values[8][1])/1000
+    julian_day = values[8][2].timetuple().tm_yday
+    print(atm_press)
+
+    measurement = ET0_evaluation.measurement(Tmin, Tmax, RHmin, RHmax, atm_press, par_avg, julian_day = julian_day)
+    measurement.set_julian_day(1) #rimuovilo, serve solo per il debug
+
+    """
+    Rs = R_n.Rs(Rs_umol_avg=par_avg)
+    ea = R_n.ea(T_min=Tmin,T_max=Tmax,RH_min=RHmin,RH_max=RHmax)
+    Ra = R_n.Ra(julian_day=0,latitude=0.70) #julian day setted to zero means that it automatically estimate it.
+    #TODO modify the latitude accordingly or get it from somewhere
+    #TODO as above, get the altitude from somewhere
+    Rs0 = R_n.Rs0(Ra=Ra,altitude=200)
+    Rn = R_n.Rn(Rs=Rs, ea=ea,Rs0 = Rs0)
+    T_mean = Tmax+Tmin/2
+    print("The estimated Rn is " + str(Rn))
+    """
+
+
+    #delta = delta(T_mean)
+    #gamma = gamma(atm_press/100)
+    pprint(vars(measurement))
+    print("ea :", measurement.ea)
+    print("Rs :", measurement.Rs)
+    print("Rs0 :", measurement.Rs0)
+    print("Rn :", measurement.Rn)
+    print("Ra :", measurement.Ra)
+    print("Delta :", measurement.delta)
+    print("gamma :", measurement.gamma)
+
+    ET0 = measurement.ET_0
+    print('\n ET0 ', ET0)
+
+
+
 
 
 
--- platform/ET0_evaluation.py
+++ platform/ET0_evaluation.py
@@ -1,10 +1,138 @@
 'This script contains the formula for the evaluation of ET0 in both cases (wind = 0, wind >0)'
+from typing import Any, Union
+
+"""script used for evaluation of R_n"""
+
+import T_mean
+import math
+import datetime
+
+class measurement(object):
+
+    def __init__(self, Tmin, Tmax, RHmin,
+                 RHmax, atm_pressure, par_avg,
+                 julian_day = 0, latitude = 0.70, altitude = 200, wind = 0):
+        self.Tmin = Tmin
+        self.Tmax = Tmax
+        self.RHmin = RHmin
+        self.RHmax = RHmax
+        self.atm_pressure = atm_pressure
+        self.T_mean = (Tmax + Tmin )/2
+        self.julian_day = julian_day
+        self.par_avg = par_avg
+        self.latitude = latitude
+        self.altitude = altitude
+        self.wind = wind
+        self.ET_0 = self.ET0()
+
+        """
+        self.gamma = self.gamma()
+        self.ea = self.ea()
+        self.Rs = self.Rs()
+        self.Ra = self.Ra()
+        self.Rs0 = self.Rs0()
+        self.Rn = self.Rn()
+        """
 
 
-def ET0(delta, R_n, gamma, T = 0, wind=0, es=0, ea=0, G=0):
-    if wind == 0:
-        #complete formula for wind =!= 0
-        ET = (0.408 * delta * (R_n - G))/(delta + gamma)
-    else:
-        ET = (0.408 * delta * (R_n - G) + (gamma * (900/(T+273)) * wind *(es - ea)))/(delta + gamma*(1 + (0.34*wind)))
-    return ET
+    @property
+    def ea(self):
+        T_min = self.Tmin
+        T_max = self.Tmax
+        RH_min = self.RHmin
+        RH_max = self.RHmax
+        et0_min = 0.618 * math.exp(17.27*T_min/(T_min+273.3))
+        et0_max = 0.618 * math.exp(17.27 * T_max / (T_max + 273.3))
+
+        ea = ((et0_min*RH_max) + (et0_max*RH_min))/100*2
+        return ea
+
+    @property
+    def Rs(self):
+        "convert Rs in MJ/m^2"
+        Rs_umol_avg = self.par_avg
+        Rs_umol_tot = Rs_umol_avg*60*60*24
+        Rs = Rs_umol_tot/4.6e6
+
+        return Rs
+
+    @property
+    def Ra(self):
+        julian_day = self.julian_day
+        latitude = self.latitude
+        "specify the julian day, otherwise we automatically obtain get it (the current one)"
+        if julian_day == 0:
+            julian_day = datetime.datetime.now().day
+
+        solar_declination = 0.409*math.sin(2*3.14*julian_day/365-1.39) #OK
+        inv_rel_dist = 1+0.033*math.cos(2*math.pi*julian_day/365) #OK
+        sunset_hour_angle = math.acos(-math.tan(latitude)*math.tan(solar_declination)) #OK
+        Ra = 24*60/math.pi*0.082*inv_rel_dist * (sunset_hour_angle * math.sin(latitude)*math.sin(solar_declination) + math.cos(latitude) * math.cos(solar_declination) * math.sin(sunset_hour_angle)) #should be OK
+        return Ra
+
+    @property
+    def Rs0(self):
+
+        Ra = self.Ra
+        altitude = self.altitude
+        Rs0 = (0.75 + 0.00002*altitude)*Ra #OK
+        return Rs0
+
+    @property
+    def Rn(self):
+
+        Rs = self.Rs
+        ea = self.ea
+        Rs0 = self.Rs0
+        T_max = self.Tmax
+        T_min = self.Tmin
+
+        #t_mean, T_min, T_max = T_mean.T_mean('2021-3-24-0-0-10', '2021-3-24-0-30-10') deprecated old method
+        Rns = (1-0.23)*Rs
+        Rnl = 4.903e-09 * (((T_min+273.16)**4+(T_max + 273.16)**4)/2) * (0.34 - (0.14 * ea ** 0.5)) * (1.35 * Rs / Rs0 - 0.35)
+        Rn = Rns-Rnl
+
+        return Rn
+
+    @property
+    def delta(self):
+        '∆ slope vapour pressure curve [kPa °C-1]'
+        T_mean = self.T_mean
+        # actually now we are passing T and not Tmean, we should work on this
+        delta = 4098 * (0.6108 * math.exp(17.27 * T_mean / (T_mean + 237.3))) / (
+                    (T_mean + 237.3) ** 2)  # formula  from excel file
+
+        return delta
+
+    @property
+    def gamma(self):
+        'psychrometric constant'
+        atm_press= self.atm_pressure
+        gamma= 0.000665 * atm_press
+
+        return gamma
+
+
+    def ET0(self, es=0, G=0):
+
+        gamma = self.gamma
+        delta = self.delta
+        R_n = self.Rn
+        T = self.T_mean
+        wind = self.wind
+        ea = self.ea
+
+        if wind == 0:
+            #complete formula for wind =!= 0
+            ET = (0.408 * delta * (R_n - G))/(delta + gamma)
+        else:
+            ET = (0.408 * delta * (R_n - G) + (gamma * (900/(T+273)) * wind *(es - ea)))/(delta + gamma*(1 + (0.34*wind)))
+
+
+        self.ET_0 = ET
+
+        return ET
+
+    def set_julian_day(self, julian_day):
+        self.julian_day = julian_day
+
