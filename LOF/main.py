ccdaf24c9bc90c8f42af87128da0b4ff638bd5bf
Amir-Sakaki <64203487+Amir-Sakaki@users.noreply.github.com>
2021-05-01 11:47:56 +0200

    save train data in db fixed bugs

--- LOF/main.py
+++ LOF/main.py
@@ -5,6 +5,7 @@ import evaluator
 import logging
 import time
 import dbconnection
+import json
 
     
 def train():
@@ -13,7 +14,10 @@ def train():
     train_data = trainModel.Train()
     train_data.fetch_data(trainInterval_h)
     train_data = train_data.train()
-        
+    
+    #save train data in db
+    saveToDB('train_input.json')
+    
     return train_data
 
 def test(train_data):
@@ -22,40 +26,51 @@ def test(train_data):
     test_data = evaluator.predict(train_data)
     test_data.fetch_data()
     result = test_data.test()
+    
+    #save novelty detection result in db
+    saveToDB('newdb_input.json')
+    
     return result
 
-#set intervals of training and testing   
-trainInterval_h = 8
-trainInterval_s = trainInterval_h*3600
-testInterval = 900
-
+def saveToDB(jsonName):
+    with open(jsonName) as infile: 
+        data = json.load(infile)
+    saveData.saveFilteredData(data)
+    
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s - %(message)s',filename="logfilename.log", level=logging.INFO)
+    
     saveData = dbconnection.db_connection()
+    
+    #set intervals of training and testing   
+    trainInterval_h = 8
+    trainInterval_s = trainInterval_h*3600
+    testInterval = 900
+    
 #%%    
-    # uncomment 3 below lines if you want to run it only once to check
-    # model = train()
-    # test_data = test(model)
-    # saveData.saveFilteredData(test_data)
+    # uncomment 2 below lines if you want to run it only once to check
+    model = train()
+    test_data = test(model)
+
 #%%
     '''
     every testInterval, latest measurements are used as
-    test data using latest trained model
+    test data and evaluated using latest trained model.
     A new model is updated every trainInterval_h 
     '''
     
-    start_time = time.time()
-    model = train()
+    # start_time = time.time()
+    # model = train()
 
-    while True:       
-        now = time.time()
-        if now - start_time >= trainInterval_s:
-            model = train()
-            start_time = time.time()
-        test_data = test(model)
-        saveData.saveFilteredData(test_data)
+    # while True:       
+    #     now = time.time()
+    #     if now - start_time >= trainInterval_s:
+    #         model = train()
+    #         start_time = time.time()
+    #     test_data = test(model)
+    #     saveData.saveFilteredData(test_data)
         
-        time.sleep(testInterval)
+    #     time.sleep(testInterval)
                 
         
 
--- LOF/trainModel.py
+++ LOF/trainModel.py
@@ -14,16 +14,15 @@ class Train(object):
     fetch data from database
     sort them alphabetically
     and make a DataFrame
-    "ANE","PLV1","PLV2","PLV3","WV" are exempt 
     """
     def fetch_data(self,trainInterval_h):
         select_train = dbconnection.db_connection()
         myresult = select_train.getSensorData(trainInterval_h)
-        temp = []
-        for result in myresult:
-            if result[0] not in ["ANE","PLV1","PLV2","PLV3","WV"]:
-                temp.append(result)
-        myresult = temp
+        # temp = []
+        # for result in myresult:
+        #     if result[0] not in ["ANE","PLV1","PLV2","PLV3","WV"]:
+        #         temp.append(result)
+        # myresult = temp
         self.sensors = [x[0] for x in myresult]
         self.sensors = np.unique(self.sensors)
         self.sensors.sort()
@@ -31,9 +30,11 @@ class Train(object):
         self.df = pd.DataFrame(myresult,columns=['sensor','value','timeStamp','ID'])
     #%%    
     """
-    train the model using last 8h sensor data except the latest sensor data
+    train the model using last trainInterval_h sensor data except the latest sensor data.
     using latest sensor data as test data in 'predictor.py'
-    output: trained model
+    "ANE","PLV1","PLV2","PLV3","WV" are exempt from model training.
+    output: trained model to be exploit in the next step by ecaluator.py
+            and a json file containing train data to be stored in db
     """   
     def train(self):
         measurements = []
@@ -45,52 +46,57 @@ class Train(object):
             locals()[str(sensor)] = self.df.loc[self.df['sensor'] == sensor]
             locals()[str(sensor)] = locals()[str(sensor)].reset_index(drop = True)
             locals()[str(sensor)] = locals()[str(sensor)][1:]
-            measurements.append(locals()[str(sensor)])
-            
-            measure = (locals()[str(sensor)])   
-            
-            #number of neighbors to be sent to LOF
-            #it has to be proportional to number of measurements
-            n_neighbors = (len(measure)-2)
-            
-            #creating a LOF class object and pass dataset to it
-            data = LOF.LOF(measure['value'],n_neighbors)
                         
-            #outlier detection of current data set
-            anomalies, inliers, _ , scores = data.run()
+            measure = (locals()[str(sensor)])   
+            if sensor in ["ANE","PLV1","PLV2","PLV3","WV"]:
+                scores = [1]*len(measure)
+                measure['isOutlier'] = scores
+                
+            else:
+                #number of neighbors to be sent to LOF
+                #it has to be proportional to number of measurements
+                n_neighbors = (len(measure)-2)
+                
+                #creating a LOF class object and pass dataset to it
+                data = LOF.LOF(measure['value'],n_neighbors)
+                            
+                #outlier detection of current data set
+                anomalies, inliers, _ , scores = data.run()
+                
+                #model training using remaining inliers from previous step
+                tModel = data.trainModel()
+                tModels.append(tModel)
+                #make a dictionary with format {sensor: model}
+                tModels_dict[sensor] = tModel
+                                
+                measure['isOutlier'] = scores
+            measurements.append(measure)
+        #save train data    
+        toSend = self.toJson(measurements)
+        saveData = dbconnection.db_connection()
             
-            #model training using remaining inliers from previous step
-            tModel = data.trainModel()
-            tModels.append(tModel)
-            #make a dictionary with format {sensor: model}
-            tModels_dict[sensor] = tModel
-            
-            #save train data
-            measure['isOutlier'] = scores
-            toSend = self.toJson(measure)
             
         print("\n\n\n\nmodel trained!\n\n\n\n")
         return tModels_dict
     #%%
-    def toJson(self,measure):
-        
-        for i,score in enumerate(measure['isOutlier']):
-            if score == 1:
-                measure['isOutlier'][i+1] = False
-            else:
-                measure['isOutlier'][i+1] = True
+    def toJson(self,measurements):
                 
         output_dict = {}
         output_list =[]
         
-        for m in range(1,len(measure)+1):
-            output_dict['sensor'] = measure['sensor'][m]
-            output_dict['value'] = measure['value'][m]
-            output_dict['timestamp']=measure['timeStamp'][m].isoformat()
-            output_dict['ID'] = str(measure['ID'][m])
-            output_dict['isOutlier'] = measure['isOutlier'][m]
-            c = output_dict.copy()
-            output_list.append(c)
+        for measure in measurements:
+            for m in range(1,len(measure)+1):
+                output_dict['sensor'] = measure['sensor'][m]
+                output_dict['value'] = measure['value'][m]
+                output_dict['timestamp']=measure['timeStamp'][m].isoformat()
+                output_dict['ID'] = str(measure['ID'][m])
+                if measure['isOutlier'][m] == 1:
+                    output_dict['isOutlier'] = False
+                else:
+                    output_dict['isOutlier'] = True
+                c = output_dict.copy()
+                output_list.append(c)
 
         with open('train_input.json', 'w') as outfile: 
             json.dump(output_list, outfile, indent = 4)
+            
