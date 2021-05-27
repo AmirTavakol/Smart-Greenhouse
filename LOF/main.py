1f5ecf4eff4f12e95c6930d42fc33afbebe919df
ANKIT <amadaan95@gmail.com>
2021-05-27 12:04:52 +0200

    Update main.py

--- LOF/main.py
+++ LOF/main.py
@@ -45,39 +45,45 @@ def saveToDB(jsonName):
     saveData.saveFilteredData(json.dumps(data))
     
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s - %(message)s',filename="logfilename.log", level=logging.INFO)
+    try:
+        logging.basicConfig(format='%(asctime)s - %(message)s',filename="logfilename.log", level=logging.INFO)
     
-    saveData = dbconnection.db_connection()
+        saveData = dbconnection.db_connection()
     
-    #set intervals of training and testing   
-    trainInterval_h = 8
-    trainInterval_s = trainInterval_h*3600
-    testInterval = 900
+        #set intervals of training and testing   
+        trainInterval_h = 8
+        trainInterval_s = trainInterval_h*3600
+        testInterval = 900
     
-#%%    
-    # uncomment 2 below lines if you want to run it only once to check
-    #model = train()
-    #test_data = test(model)
+    #%%    
+        # uncomment 2 below lines if you want to run it only once to check
+        #model = train()
+        #test_data = test(model)
 
-#%%
-    '''
-    every testInterval, latest measurements are used as
-    test data and evaluated using latest trained model.
-    A new model is updated every trainInterval_h 
-    '''
-    
-    start_time = time.time()
-    model = train()
+    #%%
+        '''
+        every testInterval, latest measurements are used as
+        test data and evaluated using latest trained model.
+        A new model is updated every trainInterval_h 
+        '''
+        
+        start_time = time.time()
+        model = train()
 
-    while True:       
-        now = time.time()
-        if now - start_time >= trainInterval_s:
-            model = train()
-            start_time = time.time()
-        test_data = test(model)
-        
-        
-        time.sleep(testInterval)
+        while True:       
+            now = time.time()
+            if now - start_time >= trainInterval_s:
+                model = train()
+                start_time = time.time()
+            test_data = test(model)
+            
+            
+            time.sleep(testInterval)
+
+    except Exception as ex:
+        logging.info(ex)
+        raise Exception
+
                 
         
 
