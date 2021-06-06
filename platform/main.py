aaca5e4019389a79e65a64e91f22ef956b437ebc
Francesco Tosetti <frc.tst97@gmail.com>
2021-06-06 21:30:55 +0200

    corrected json paths

--- platform/main.py
+++ platform/main.py
@@ -4,9 +4,9 @@ import ET0
 import ETc
 import trigger
 
-schedule.every().day.at("16:30").do(ET0.ET0_2_db)
-schedule.every().day.at("16:35").do(ETc.saveETc)
-schedule.every().day.at("16:40").do(trigger.trigger)
+schedule.every().day.at("21:38").do(ET0.ET0_2_db)
+schedule.every().day.at("21:39").do(ETc.saveETc)
+schedule.every().day.at("21:40").do(trigger.trigger)
 
 
 while True:
